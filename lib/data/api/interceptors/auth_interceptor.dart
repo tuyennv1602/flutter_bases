31331b78d7b7e3cac2b0d8cb78481f2e758b9aab
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-03-16 14:30:12 +0700

    local

--- lib/data/api/interceptors/auth_interceptor.dart
+++ lib/data/api/interceptors/auth_interceptor.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_base/data/local_datasource/token_provider.dart';
+
+import '../../data_source/local_datasource/token_provider.dart';
 
 class AuthInterceptor extends Interceptor {
   final TokenProvider _tokenProvider;
---
+++ lib/domain/usecases/demo_usecase.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_base/domain/reponsitories/demo_repository.dart';
+
+class DemoUsecase {
+  final DemoRepository _demoRepository;
+
+  DemoUsecase(this._demoRepository);
+}
