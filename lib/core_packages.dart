e8650b22624ec6f040eb2d428772878020e86b39
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-03-16 15:41:09 +0700

    update export core

--- lib/core_packages.dart
+++ lib/core_packages.dart
@@ -2,3 +2,4 @@ export 'package:flutter_base/styles.dart';
 export 'package:flutter_base/theme.dart';
 export 'package:flutter_base/routing/routing.dart';
 export 'package:flutter_base/localizations/app_localizations.dart';
+export 'core/core.dart';
